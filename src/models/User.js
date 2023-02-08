b98edd245d4e79bd900b7cd0cd94abec8806ca0a
gus-tavo01 <gustavoa.loera01@gmail.com>
2023-02-08 08:52:07 -0800

    add user model schema

---
+++ src/models/User.js
@@ -0,0 +1,51 @@
+import { Schema, model } from 'mongoose';
+// paginate
+import roles from '../constants/roles';
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createDate: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
+  modifiedDate: {
+    type: Date,
+    required: false,
+  },
+  active: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+  // permissions: [{ type: String, required: true }], TBD
+  role: {
+    type: String,
+    required: true,
+    default: roles.USER,
+  },
+  englishLevel: {
+    type: String,
+    required: false,
+  },
+  techSkills: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
+  resumeLink: { type: String, required: false },
+});
+
+export default model('User', UserSchema);

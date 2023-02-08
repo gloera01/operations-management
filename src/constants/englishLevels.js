9068f2f25bcdaa9d693236dae6e6cfa5cab31d93
gus-tavo01 <gustavoa.loera01@gmail.com>
2023-02-08 08:58:32 -0800

    add constants files

---
+++ src/constants/englishLevels.js
@@ -0,0 +1,5 @@
+export const BASIC = 'basic';
+export const MEDIUM = 'medium';
+export const ADVANCED = 'advanced';
+
+export default { BASIC, MEDIUM, ADVANCED };
---
+++ src/constants/roles.js
@@ -0,0 +1,5 @@
+export const SUPERADMIN = 'superAdmin';
+export const ADMIN = 'admin';
+export const USER = 'normal';
+
+export default { SUPERADMIN, ADMIN, USER };

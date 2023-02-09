24d905c3e33aaf357db799ac5a783bda5df57055
gus-tavo01 <gustavoa.loera01@gmail.com>
2023-02-09 11:52:07 -0800

    add withHttpResponse middleware on login controller

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import controller from './controller';
 
-const router = Router().post('/login', controller.login);
+import withHttpResponse from '../../middlewares/withHttpResponse';
+
+const router = Router().post('/login', withHttpResponse(controller.login));
 
 export default router;

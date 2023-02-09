49910ededff6305d5bc48ef8b3045683a8c3a65a
gus-tavo01 <gustavoa.loera01@gmail.com>
2023-02-09 10:59:13 -0800

    add users post endpoint

--- src/routes/users/controller.js
+++ src/routes/users/controller.js
@@ -1,20 +1,27 @@
-import HttpResponseHandler from '../../commons/httpResponseHandler';
+import httpResponseHandler from '../../commons/httpResponseHandler';
 import UserModel from '../../models/User';
+import passwordService from '../../services/passwordService';
 import signupServiceFactory from '../../services/signupServiceFactory';
 
-export const create = async (req, res) => {
-  const httpResponse = new HttpResponseHandler(res);
+export const create = async (req) => {
   try {
     const { role: userType } = req.body;
     const signupService = signupServiceFactory.createService(
       userType,
-      UserModel
+      UserModel,
+      httpResponseHandler,
+      passwordService
     );
-    return signupService.signup(req.body);
+    const response = await signupService.signup(req.body);
+
+    // TODO: implement logger
+    console.log(response);
+    return response;
   } catch (error) {
     // TODO: implement logger
     console.log('Server error');
-    return httpResponse.serverError(error);
+    console.log(error.stack);
+    return httpResponseHandler.serverError(error.message);
   }
 };
 

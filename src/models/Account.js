fb96146a024153b6e675a81dd387a3ce51110ec7
Gustavo Loera <gustavo.loera@arkusnexus.com>
2023-02-10 16:21:48 -0800

    modify operationsManager prop from account schema

--- src/models/Account.js
+++ src/models/Account.js
@@ -1,10 +1,10 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, ObjectId } from 'mongoose';
 import AccountMember from './schemas/AccountMember';
 
 const Account = new Schema({
   name: { type: String, required: true },
   client: { type: String, required: true },
-  operationsManager: { type: AccountMember, required: true },
+  operationsManager: { type: ObjectId, required: true, ref: 'User' },
   members: [AccountMember],
 });
 

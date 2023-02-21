2bf246c61547ff4198eba88e78030e4016806d7b
Gustavo Loera <gustavo.loera@arkusnexus.com>
2023-02-21 11:03:46 -0800

    user model map name anad email to lowercase

--- src/models/User.js
+++ src/models/User.js
@@ -6,10 +6,12 @@ const UserSchema = new Schema({
   name: {
     type: String,
     required: true,
+    lowercase: true,
   },
   email: {
     type: String,
     required: true,
+    lowercase: true,
   },
   password: {
     type: String,

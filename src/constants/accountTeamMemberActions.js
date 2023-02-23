f9082752fb84b8eec41f424fce800f4616701898
Gustavo Loera <gustavo.loera@arkusnexus.com>
2023-02-23 15:09:39 -0800

    add actions constants

---
+++ src/constants/accountTeamMemberActions.js
@@ -0,0 +1,6 @@
+export const ADDED = 'has been added to the team successfully.';
+export const REMOVED = 'has ben removed from the team.';
+
+const ACCOUNT_TEAM_MEMBER_ACTIONS = { ADDED, REMOVED };
+
+export default ACCOUNT_TEAM_MEMBER_ACTIONS;

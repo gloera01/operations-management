d90de38d5e69efa8ece06b8124b0f5e8d7d24084
Gustavo Loera <gustavo.loera@arkusnexus.com>
2023-02-23 16:10:19 -0800

    add pagination plugin into account model

--- src/models/Account.js
+++ src/models/Account.js
@@ -1,11 +1,16 @@
 import { Schema, model, ObjectId } from 'mongoose';
 import AccountMember from './schemas/AccountMember';
+import mongoosePaginate from 'mongoose-paginate-v2';
 
 const Account = new Schema({
   name: { type: String, required: true },
   client: { type: String, required: true },
   operationsManager: { type: ObjectId, required: true, ref: 'User' },
   members: [AccountMember],
+  createDate: { type: Date, required: false, default: Date.now },
+  modifiedDate: { type: Date, required: false },
 });
+
+Account.plugin(mongoosePaginate);
 
 export default model('Account', Account);

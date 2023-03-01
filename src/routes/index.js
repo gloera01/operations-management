9aec571fe5450ca6434666bce7c7aab38ce74c37
Gustavo Loera <gustavo.loera@arkusnexus.com>
2023-02-28 17:55:36 -0800

    add operations history endpoint into router

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import auth from './auth';
 import users from './users';
 import accounts from './accounts';
 import accountMemmbers from './accountMembers';
+import operationsHistory from './operationsHistory';
 
 const routes = Router();
 
@@ -13,5 +14,6 @@ routes.use(`${basePath}/auth`, auth);
 routes.use(`${basePath}/users`, users);
 routes.use(`${basePath}/accounts`, accounts);
 routes.use(`${basePath}/accounts/:accountId/members`, accountMemmbers);
+routes.use(`${basePath}/accounts/:accountId/operations`, operationsHistory);
 
 export default routes;

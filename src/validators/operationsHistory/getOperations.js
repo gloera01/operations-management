0799e81499e8c31aabbdbf68c8a9cdc1bf5a44c0
Gustavo Loera <gustavo.loera@arkusnexus.com>
2023-03-01 15:47:12 -0800

    update getOperations validator

--- src/validators/operationsHistory/getOperations.js
+++ src/validators/operationsHistory/getOperations.js
@@ -5,8 +5,8 @@ export default Joi.object({
   pageSize: Joi.number().greater(0),
   sortOrder: Joi.string().valid('asc', 'desc'),
   createDate: Joi.date(),
-  accountName: Joi.string(),
   memberStartDate: Joi.date(),
   memberEndDate: Joi.date(),
   memberName: Joi.string(),
+  memberEmail: Joi.string(),
 });
